9e6eda67c3cd86b3bfa323b8cd21d4a2784efbfc
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-07-19 05:01:37 +0200

    Angies solution to the previous assignment

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-setInterval(() => {
-        let currentCount = document.getElementById("textarea");
-        let currentCountLength = currentCount.value.length;
-        let printCurrentCount = document.getElementById("output");
-
-        printCurrentCount.innerHTML = `You have written ${currentCountLength} characters, you have ${200 - currentCountLength} characters left.`;
-
-        if((200 - currentCountLength) < 0) {
-            currentCount.style.outline = "3px solid red";
-        } else {
-            currentCount.style.outline = "none";
-        }
-        }
-, 100);
+// Angela's solution
+let yourInput = prompt(`Enter your text here.`);
+alert(`You have written ${yourInput.length} characters, you have ${200 - yourInput.length} characters left.`)
