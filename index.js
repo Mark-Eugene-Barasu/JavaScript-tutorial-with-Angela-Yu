ade3b0fae79d3b6197aca8864c0207c29237adbe
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-07-19 04:43:10 +0200

    completed assignment on word countdown :)

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-var message = "Hello";
-var name = "Mark";
+setInterval(() => {
+        let currentCount = document.getElementById("textarea");
+        let currentCountLength = currentCount.value.length;
+        let printCurrentCount = document.getElementById("output");
 
-alert(`${message} there, ${name}`);
+        printCurrentCount.innerHTML = `You have written ${Number(currentCountLength)} characters, you have ${200 - Number(currentCountLength)} characters left.`;
+
+        if((200 - currentCountLength) < 0) {
+            currentCount.style.outline = "3px solid red";
+        } else {
+            currentCount.style.outline = "none";
+        }
+        }
+, 100);
