25474a2460570aa39e9d587afd8530d7abe3e84e
Eugene Mark Korku Barasu <70624924+Mark-Eugene-Barasu@users.noreply.github.com>
2024-07-20 07:26:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-// life in weeks challenge
-
-let age  = document.getElementById("inputAge").value;
-
-function lifeInWeeks(age) {
-    let lifeInWeeksLeft = 90 - Number(age);
-    let days = lifeInWeeksLeft * 365;
-    let weeks = lifeInWeeksLeft * 52;
-    let months = lifeInWeeksLeft * 12;
-
-    document.querySelector(".outputResult").innerHTML = `You are left with ${days} days, ${weeks} weeks and ${months} months left.`
+// Life in weeks challenge
+function lifeInWeeks() {
+            let age = document.getElementById("inputAge").value;
+            let lifeInWeeksLeft = 90 - Number(age);
+            let days = lifeInWeeksLeft * 365;
+            let weeks = lifeInWeeksLeft * 52;
+            let months = lifeInWeeksLeft * 12;
+            document.querySelector(".outputResult").innerHTML = `You are left with ${days} days, ${weeks} weeks, and ${months} months left.`;
 }
+    
