eb6fee4e46a7f291c03855e76131dff264861561
Eugene Mark Korku Barasu <mark0630227243@gmail.com>
2024-07-27 19:26:22 +0200

    random person picker complete successful

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
-// FizzBuzz quiz 
 
-let numbers = [];
+let people = [`Angela`, `Ben`, `Jerry`, `Micheal`, `Chloe`];
 
-for(let i = 1; i <= 100; i++){
-    if (i % 3 === 0 && i % 5 === 0) {
-        let i = `FizzBuzz`;
-        numbers.push(i);
-    } else if (i % 3 === 0) {
-        let i =  `Fizz`;
-        numbers.push(i);
-    } else if (i % 5 === 0) {
-        let i = `Buzz`;
-        numbers.push(i);
-    } else {
-        // i = i;
-        numbers.push(i);
-    }
-}
+let randomPick  = Math.floor(Math.random() * people.length)
 
-console.log(numbers);
+console.log(`${people[randomPick]}, is paying for dinner today.`);
