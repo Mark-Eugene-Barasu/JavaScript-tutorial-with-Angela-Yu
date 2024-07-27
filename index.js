1e1930626ddc8c0b758dafe20787b9d18624d3ac
Eugene Mark Korku Barasu <mark0630227243@gmail.com>
2024-07-27 05:28:43 +0200

    love calculator complete.

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-// BMI calculator
+// love calculator
 
-function bmiCalculator(weight, height) {
-    return Math.round( weight / Math.pow(height, 2));
+let firstUser = prompt(`What is your name?:`);
+let secondUser = prompt(`Who is your crush?:`);
+
+// random numbers between 1 - 100
+let randomPerCentage = Math.round(Math.random() * 100) + 1
+
+if (randomPerCentage >= 70) {
+    alert(`The love percentage between ${firstUser} ❤️ ${secondUser} \n${randomPerCentage}% \n You love each other like Romeo loves Juliet.`);
+} else if (randomPerCentage > 30 && randomPerCentage <= 70) {
+    alert(`The love percentage between ${firstUser} ❤️ ${secondUser} \n${randomPerCentage}%`);
+} else {
+    alert(`The love percentage between ${firstUser} ❤️ ${secondUser} \n${randomPerCentage}% \n You'll go together like water and oil.`);
 }
-
-console.log(bmiCalculator(65, 1.8));
