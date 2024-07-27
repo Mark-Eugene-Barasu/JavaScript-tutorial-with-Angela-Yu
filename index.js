a2ae8612f54c5cb7af97f4bb1ac564e3686899fc
Eugene Mark Korku Barasu <mark0630227243@gmail.com>
2024-07-27 06:21:22 +0200

    Leap Year Challenge Completed

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-// love calculator
+// Leap Year Calculator
 
-let firstUser = prompt(`What is your name?:`);
-let secondUser = prompt(`Who is your crush?:`);
-
-// random numbers between 1 - 100
-let randomPerCentage = Math.round(Math.random() * 100) + 1
-
-
-if (randomPerCentage >= 70) {
-    alert(`The love percentage between ${firstUser} ❤️ ${secondUser} \n${randomPerCentage}% \n You love each other like Romeo loves Juliet.`);
-} else if (randomPerCentage > 30 && randomPerCentage <= 70) {
-    alert(`The love percentage between ${firstUser} ❤️ ${secondUser} \n${randomPerCentage}%`);
-} else {
-    alert(`The love percentage between ${firstUser} ❤️ ${secondUser} \n${randomPerCentage}% \n You'll go together like water and oil.`);
+function learYear(year) {
+    if ((year % 4) === 0) {
+        if ((year % 100) === 0) {
+            if ((year % 400) === 0) {
+                return `Leap year`;
+            } else {
+                return `Not Leap Year`;
+            }
+        } else {
+            return  `Leap Year`;
+        }
+    } else {
+        return `Not Leap Year`;
+    }
 }
-
